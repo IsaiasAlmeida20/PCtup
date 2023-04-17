3b217b1276136711cb38579a1595765374ee203a
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-04-17 18:25:55 -0300

    :tada: inital commit

---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-app-bar flat>
+      <v-container class="fill-height d-flex align-center">
+        
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-btn
+                  v-for="link in links"
+                  :key="link"
+                  variant="text"
+                >
+                  {{ link }}
+                </v-btn>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
+  }
+</script>
