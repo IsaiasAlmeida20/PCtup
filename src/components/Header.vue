a7d885c0d692499d0d9c433b20bba2773cf52b88
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-04-21 16:34:17 -0300

    :recycle: refactor: header and select font

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-app-bar 
-    :elevation="10"
-    class="header bg-blue-grey-darken-4"  
-  >
-    <v-img 
-      src="./../assets/logo.png"
-      class="w-100 p-0"  
-    />
-  </v-app-bar>
+  <header class="header bg-blue-grey-darken-4">
+    <img src="./../assets/logo.png" alt="PCTup">
+    <router-link to="/login">
+      <button class="btn bg-deep-purple-accent-4">
+        <v-icon icon="mdi-login" size="32"/>
+        ENTRAR
+      </button>
+    </router-link>
+  </header>
 </template>
 
 <style scoped>
  .header {
   height: 80px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
  }
+
+ .header img{
+  width: 12rem;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+ }
+
+ .header a{
+  text-decoration: none;
+ }
+
+ .btn {
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3px;
+  font-weight: bold;
+  font-size: 1.7rem;
+  border: none;
+  border-radius: 8px;
+  width: 10rem;
+  height: 3.5rem;
+}
 </style>
