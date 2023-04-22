44105191d88ff36e06161d2f5c1749703ca291b8
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-04-22 15:45:34 -0300

    :recycle: refactor: add update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,32 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './../views/Home.vue'
-import Login from './../views/login.vue'
+import LoginView from '../views/LoginView.vue'
+import Login from '@/components/Login.vue'
+import Register from '@/components/Register.vue'
 
 const routes = [
   {
     path: '/',
+    name: 'home',
     component: Home
   },
   {
     path: '/login',
-    component: Login
+    name: 'login-view',
+    component: LoginView,
+    children: [
+      {
+        path: '',
+        name: 'login',
+        component: Login
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: Register
+      },
+    ]
   },
 ]
 
