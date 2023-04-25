6c63314b0a97b1d1537d32ba790c3616992eb738
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-04-25 17:47:09 -0300

    :sparkles: feat: init implemt routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,38 @@ import Home from './../views/Home.vue'
 import LoginView from '../views/LoginView.vue'
 import Login from '@/components/Login.vue'
 import Register from '@/components/Register.vue'
+import Post from '@/components/Post.vue'
+/* import NewSetup from '@/components/NewSetup.vue'
+import Favorites from '@/components/Favorites.vue'
+import MySetups from '@/components/MySetups.vue' */
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '',
+        name: 'posts',
+        component: Post
+      }/* ,
+      {
+        path: '/new-setup',
+        name: 'new-setup',
+        component: NewSetup
+      },
+      {
+        path: '/favorites',
+        name: 'favorites',
+        component: Favorites
+      },
+      {
+        path: '/my-setups',
+        name: 'my-setups',
+        component: MySetups
+      }, */
+    ]
   },
   {
     path: '/login',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,38 +4,44 @@
     <v-main>
       <v-container align="center">
         <div 
-          class="d-flex flex-column h-100"
+          class="container d-flex flex-column h-100"
+          align="center"
         >
           <div class="mb-4">
             <v-tabs
+              fixed-tabs
               color="blue-grey-lighten-2"
               align-tabs="center"
             >
               <v-tab :value="1">
-                <v-icon icon="mdi-home-outline" size="28" title="Novo Setup"/>
-                <span class="hidden-sm-and-down ms-1"> Início</span>
+                <router-link to="">
+                  <v-icon icon="mdi-home-outline"  size="large" title="Novo Setup"/>
+                  <span class="hidden-sm-and-down ms-1"> Início</span>
+                </router-link>
               </v-tab>
               <v-tab :value="2">
-                <v-icon icon="mdi-image-plus-outline" size="28" title="Novo Setup"/>
-                <span class="hidden-sm-and-down ms-1"> Novo Setup</span>
+                <router-link to="/new-setup">
+                  <v-icon icon="mdi-image-plus-outline"  size="large" title="Novo Setup"/>
+                  <span class="hidden-sm-and-down ms-1"> Novo Setup</span>
+                </router-link>
               </v-tab>
               <v-tab :value="3">
-                <v-icon icon="mdi-star-outline" size="28" title="Favoritos"/>
-                <span class="hidden-sm-and-down ms-1"> Favoritos</span>
+                <router-link to="/favorites">
+                  <v-icon icon="mdi-star-outline"  size="large" title="Favoritos"/>
+                  <span class="hidden-sm-and-down ms-1"> Favoritos</span>
+                </router-link>
               </v-tab>
               <v-tab :value="4">
-                <v-icon icon="mdi-monitor" size="28" title="Meus Setups"/>
-                <span class="hidden-sm-and-down ms-1"> Meus Setups</span>
+                <router-link to="/my-setups">
+                  <v-icon icon="mdi-monitor"  size="large" title="Meus Setups"/>
+                  <span class="hidden-sm-and-down ms-1"> Meus Setups</span>
+                </router-link>
               </v-tab>
             </v-tabs>
           </div>
           <div>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-          </div>
-          
+            <router-view></router-view>
+          </div> 
         </div>
       </v-container>
     </v-main>
@@ -44,12 +50,23 @@
 
 <script lang="ts" setup>
 import Header from '@/components/Header.vue';
-//import Sidebar from '@/components/Sidebar.vue'
-import Post from '@/components/Post.vue'
 
 </script>
 
 
 <style lang="css" scoped>
+  .container {
+    width: 1200px;
+  }
 
+  .container a{
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  @media (max-width: 1300px) {
+    .container{
+      width: 100%;
+    }
+  }
 </style>
