940efd5df09ef6d4289d8c0d174f5dbef28bcc8c
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-04-26 17:43:42 -0300

    :wrench:	chore: config pinia

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -8,6 +8,7 @@
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import router from '../router'
+import pinia from '../store'
 
 // Types
 import type { App } from 'vue'
@@ -17,4 +18,5 @@ export function registerPlugins (app: App) {
   app
     .use(vuetify)
     .use(router)
+    .use(pinia)
 }
