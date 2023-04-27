64d027d0e66c40c77b6664c69d54a67ce65bbef2
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-04-27 17:02:32 -0300

    :wrench:	chore: config autentication

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosInstance } from 'axios'
 
+const auth = localStorage.getItem('accessToken')
+
 const api: AxiosInstance = axios.create({
   baseURL: 'http://localhost:3333/api/v1',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    Authorization: auth
   }
 })
 
--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,32 @@
-// Utilities
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
-  }),
+export const userAuthStore = defineStore('auth', () => {
+  const accessToken = ref(localStorage.getItem('accessToken'))
+  const userId = ref(localStorage.getItem('userId'))
+
+  function setAcessToken(tokenValue: string) {
+    localStorage.setItem('accessToken', tokenValue)
+    accessToken.value = tokenValue
+  }
+
+  function setUserId(idValue: string) {
+    localStorage.setItem('userId', idValue)
+    userId.value = idValue
+  }
+
+  function getAccessToken() {
+    return accessToken.value
+  }
+
+  function getUserId() {
+    return userId.value
+  }
+
+  return {
+    getAccessToken,
+    getUserId,
+    setAcessToken,
+    setUserId
+  }
 })
