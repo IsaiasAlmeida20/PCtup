f091193d4f376b7d325c5c1def6b5aa37825e5ce
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-05-16 18:00:16 -0300

    :recycle: refactor: adjustments in setup description

--- src/components/new-setup/SetupDescription.vue
+++ src/components/new-setup/SetupDescription.vue
@@ -1,33 +1,66 @@
 <template>
   <div>
     <v-card
-      elevation="10"
-      class="bg-blue-grey-darken-4 mt-16"
-      rounded="lg"
+      v-if="!auth.getAccessToken()"
+      class="bg-blue-grey-darken-4"
     >
-    <h1 class="text-h5 ma-4">Títlo e descrição</h1>
-      <form 
-        class="ma-4" 
-      >
-        <v-text-field
-          label="Titulo"
-          v-model="setupDescription.titulo"
-          required
-        />
-    
-        <v-textarea 
-          label="Descrição"
-          v-model="setupDescription.descricao"
-          required
-        />
-      </form>
-      <v-btn
-        class="bg-deep-purple-accent-4 float-end ma-4"
-        @click="send"
-      >
-        Continuar
-      </v-btn>
-    </v-card>
+    <v-card-item>
+        <div class="text mb-1">
+         Entre ou Cadastre-se
+        </div>
+    </v-card-item>
+
+    <v-card-actions class="d-flex justify-center">
+      <router-link to="/login">
+        <v-btn 
+          class="bg-deep-purple-accent-4 ma-4"
+          color="white"
+          elevation="10"
+        >
+          Entre
+        </v-btn>
+      </router-link>
+      <router-link to="/register">
+        <v-btn 
+          class="bg-deep-purple-accent-4 ma-4"
+          color="white"
+          elevation="10"
+        >
+          Cadastre-se
+        </v-btn>
+      </router-link>
+    </v-card-actions>
+  </v-card>
+
+  <v-card
+    v-else
+    elevation="10"
+    class="bg-blue-grey-darken-4 mt-16"
+    rounded="lg"
+  >
+  <h1 class="text-h5 ma-4">Títlo e descrição</h1>
+    <form 
+      class="ma-4" 
+    >
+      <v-text-field
+        label="Titulo"
+        v-model="setupDescription.titulo"
+        required
+      />
+  
+      <v-textarea 
+        label="Descrição"
+        v-model="setupDescription.descricao"
+        required
+      />
+    </form>
+    <v-btn
+      class="bg-deep-purple-accent-4 float-end ma-4"
+      @click="send"
+    >
+      Continuar
+    </v-btn>
+  </v-card>
   </div>
 </template>
 
