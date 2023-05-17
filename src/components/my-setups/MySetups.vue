00438f59f58170db6776b1125c62e8a274068869
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-05-16 21:27:04 -0300

    :sparkles: feat: init listing setups user

--- src/components/my-setups/MySetups.vue
+++ src/components/my-setups/MySetups.vue
@@ -1,12 +1,44 @@
 <template>
-  <h1 class="text-h5 mb-4">Meus Setups</h1>
+    <!-- <div>
+    <v-progress-circular
+      class="mt-10"
+      v-if="postData.length === 0"
+      indeterminate
+      color="primary"
+    />
+    <Setup 
+      v-for="post in postData"
+      :key="post.id"
+      :imagens="post.imagens"
+      :created-at="post.createdAt"
+      :nome="post.usuario.nome"
+      :avatar="post.usuario.imagem.url"
+    />
+  </div> -->
 </template>
 
 <script setup lang="ts">
-//import Setup from '../setup/Setup.vue';
+// import { reactive, onMounted } from 'vue'
+// import api from '@/services/api'
+// import { PostType } from '@/types/comonTypes'
+// import { userAuthStore } from '@/store/app'
+// import Setup from '@/components/setup/Setup.vue'
+
+// const auth = userAuthStore()
+
+// const postData = reactive<PostType[]>([])
+
+// async function getSetups() {
+//   try {
+//     const response = await api.get<PostType[]>(`/setups/users/${auth.getUserId()}`);
+//     console.log(response.data)
+//     postData.splice(0, postData.length, ...response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// onMounted(getSetups)
 
 </script>
 
-<style scoped>
-
-</style>
