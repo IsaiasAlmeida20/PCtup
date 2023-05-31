3cb0927ab383fda4641a991ec4fccf7255bf9818
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-05-31 20:22:15 -0300

    :recycle: refactor: update setupDescription

--- src/components/new-setup/SetupDescription.vue
+++ src/components/new-setup/SetupDescription.vue
@@ -1,66 +1,41 @@
 <template>
   <div>
-    <v-card
-      v-if="!auth.getAccessToken()"
-      class="bg-blue-grey-darken-4"
-    >
-    <v-card-item>
-        <div class="text mb-1">
-         Entre ou Cadastre-se
-        </div>
-    </v-card-item>
+    <div v-if="auth.getAccessToken()">
+      <v-card
+        elevation="10"
+        class="bg-blue-grey-darken-4 mt-16"
+        rounded="lg"
+      >
+      <h1 class="text-h5 ma-4">Títlo e descrição</h1>
+        <form 
+          class="ma-4" 
+        >
+          <v-text-field
+            label="Titulo"
+            v-model="setupDescription.titulo"
+            required
+          />
+      
+          <v-textarea 
+            label="Descrição"
+            v-model="setupDescription.descricao"
+            required
+          />
+        </form>
+        <v-btn
+          class="bg-deep-purple-accent-4 float-end ma-4"
+          @click="send"
+        >
+          Continuar
+        </v-btn>
+      </v-card>
+    </div> 
 
-    <v-card-actions class="d-flex justify-center">
+    <div v-else class="link mt-16">
       <router-link to="/login">
-        <v-btn 
-          class="bg-deep-purple-accent-4 ma-4"
-          color="white"
-          elevation="10"
-        >
-          Entre
-        </v-btn>
+        Fazer Login
       </router-link>
-      <router-link to="/register">
-        <v-btn 
-          class="bg-deep-purple-accent-4 ma-4"
-          color="white"
-          elevation="10"
-        >
-          Cadastre-se
-        </v-btn>
-      </router-link>
-    </v-card-actions>
-  </v-card>
-
-  <v-card
-    v-else
-    elevation="10"
-    class="bg-blue-grey-darken-4 mt-16"
-    rounded="lg"
-  >
-  <h1 class="text-h5 ma-4">Títlo e descrição</h1>
-    <form 
-      class="ma-4" 
-    >
-      <v-text-field
-        label="Titulo"
-        v-model="setupDescription.titulo"
-        required
-      />
-  
-      <v-textarea 
-        label="Descrição"
-        v-model="setupDescription.descricao"
-        required
-      />
-    </form>
-    <v-btn
-      class="bg-deep-purple-accent-4 float-end ma-4"
-      @click="send"
-    >
-      Continuar
-    </v-btn>
-  </v-card>
+    </div>
   </div>
 </template>
 
@@ -92,3 +67,10 @@ async function send(){
 }
 
 </script>
+
+<style>
+.link a {
+  color: #FFFFFF;
+}
+
+</style>
