f8c1ccc1934a2d904cc424c5a2bd4183f647bf51
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-05-31 20:10:10 -0300

    :recycle: refactor: update mysetups

--- src/components/my-setups/MySetups.vue
+++ src/components/my-setups/MySetups.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <div>
+  <div v-if="auth.getAccessToken()">
     <v-progress-circular
       class="mt-10"
       v-if="postData.length === 0"
@@ -14,31 +14,43 @@
       :nome="post.usuario.nome"
       :avatar="post.usuario.imagem.url"
     />
-  </div> -->
+  </div>
+  <div v-else class="link mt-16">
+    <router-link to="/login">
+      Fazer Login
+    </router-link>
+  </div>
 </template>
 
 <script setup lang="ts">
-// import { reactive, onMounted } from 'vue'
-// import api from '@/services/api'
-// import { PostType } from '@/types/comonTypes'
-// import { userAuthStore } from '@/store/app'
-// import Setup from '@/components/setup/Setup.vue'
+import { reactive, onMounted } from 'vue'
+import api from '@/services/api'
+import { PostType } from '@/types/comonTypes'
+import { userAuthStore } from '@/store/app'
+import Setup from '@/components/setup/Setup.vue'
 
-// const auth = userAuthStore()
+const auth = userAuthStore()
 
-// const postData = reactive<PostType[]>([])
+const postData = reactive<PostType[]>([])
 
-// async function getSetups() {
-//   try {
-//     const response = await api.get<PostType[]>(`/setups/users/${auth.getUserId()}`);
-//     console.log(response.data)
-//     postData.splice(0, postData.length, ...response.data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+async function getSetups() {
+  try {
+    const response = await api.get<PostType[]>(`/setups/users/${auth.getUserId()}`);
+    console.log(response.data)
+    postData.splice(0, postData.length, ...response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// onMounted(getSetups)
+onMounted(getSetups)
 
 </script>
+
+<style>
+.link a {
+  color: #FFFFFF;
+}
+
+</style>
 
