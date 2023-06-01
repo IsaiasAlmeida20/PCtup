154aaccd5ff10f856436efc46a9fb808590795e5
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-05-31 22:49:03 -0300

    :recycle: refactor: update header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -5,24 +5,41 @@
     justify="space-around"
   >
     <v-app-bar-title>
-      <v-img
-        class="ms-sm-16 me-8"
-        :width="140"
-        aspect-ratio="16/9"
-        src="@/assets/logo.png"
-      >
-      </v-img>
+      <router-link to="/">
+        <v-img
+          class="ms-sm-16 me-8"
+          :width="140"
+          aspect-ratio="16/9"
+          src="@/assets/logo.png"
+        />
+      </router-link>
     </v-app-bar-title>
     <div
+      v-if="!auth.getAccessToken()"
       class="pa-3 bg-deep-purple-accent-4 rounded-circle elevation-10 me-sm-16 me-8"
     >
       <router-link to="/login"  class="text-decoration-none">
         <v-icon icon="mdi-account-outline" color="white"/>
       </router-link>
     </div>
+    <div
+      v-else
+      class=" rounded-circle elevation-10 me-sm-16 me-8"
+    >
+      <router-link to="/profile">
+        <v-avatar size="48">
+          <v-img src="https://github.com/isaiasalmeida20.png"/>
+        </v-avatar>
+      </router-link>
+    </div>
   </v-app-bar>
 </template>
 
-<style lang="css" scoped>
+<script setup lang="ts">
+import { userAuthStore } from '@/store/app'
 
-</style>
+const auth = userAuthStore()
+
+const image = auth.getUserImg()
+
+</script>
