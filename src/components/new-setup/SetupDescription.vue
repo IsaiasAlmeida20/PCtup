6c0f354924fcbd6bed6446b90114ffc2bde747e4
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-07-08 10:53:03 -0300

    :recycle: refactor: adjustments in post setup

--- src/components/new-setup/SetupDescription.vue
+++ src/components/new-setup/SetupDescription.vue
@@ -21,18 +21,39 @@
             v-model="setupDescription.descricao"
             required
           />
+
           <v-file-input 
-            @update:model-value="previewUrl = ''"
+            @update:model-value="previewUrl!"
             type="file" 
             ref="fileInput" 
-            label="Selecione uma imagem"
+            label="Selecione as imagens do setup (uma por vez)"
             prepend-icon="mdi-camera"
             @change="previewImage" 
           />
-          <v-img 
-            class="w-75 mb-10 rounded-lg"
-            :src="previewUrl" 
-          />
+          <v-row>
+            <v-col
+              v-for="(img, i) in imagens"
+              :key="i"
+              class="d-flex child-flex"
+              cols="4"
+              >
+                  <v-img
+                      :src="img"
+                      aspect-ratio="1"
+                      cover
+                      class="bg-grey-lighten-2 rounded-lg "
+                  >
+                  <v-btn 
+                      class="btn" 
+                      elevation="0" 
+                      size="small" 
+                      icon="mdi-trash-can" 
+                      color="transparent"
+                      @click="removeItem(i)"
+                  />
+                  </v-img>
+            </v-col>
+          </v-row>
         </form>
         <v-btn
           class="bg-deep-purple-accent-4 float-end ma-4"
@@ -42,6 +63,23 @@
           Salvar
         </v-btn>
       </v-card>
+      <v-snackbar
+        v-model="snackbar"
+        :timeout="6000"
+        color="blue-grey"
+      >
+        Seu setup foi postado com sucesso!
+        Aguarde até que um dos administradores aprovem seu post.
+        Enquanto isso podera vê-lo em meus setups.
+        <template v-slot:actions>
+          <v-btn
+            color="blue"
+            variant="text"
+            icon="mdi-close"
+            @click="snackbar = false"
+          />
+        </template>
+      </v-snackbar>
     </div> 
 
     <div v-else class="link mt-16">
@@ -61,8 +99,11 @@ import { SetupDescription } from '@/types/comonTypes'
 
 const auth = userAuthStore()
 const fileInput = ref<any>();
+const files = ref<any[]>([]);
 const previewUrl = ref<any>();
 const loading = ref<boolean>(false)
+const snackbar = ref<boolean>(false)
+const imagens = ref<any[]>([])
 
 const setupDescription = reactive<SetupDescription>({
   usuarioId: auth.getUserId(),
@@ -70,11 +111,18 @@ const setupDescription = reactive<SetupDescription>({
   descricao: ''
 })
 
+function removeItem(index: any){
+  files.value.splice(index, 1);
+  imagens.value.splice(index, 1);
+};
+
 function previewImage(): void{
   const file = fileInput.value.files[0];
+  files.value.push(file)
   const reader = new FileReader()
   reader.onload = () => {
     previewUrl.value = reader.result;
+    imagens.value.push(previewUrl.value)
   }
   if (file) {
     reader.readAsDataURL(file)
@@ -86,23 +134,36 @@ async function send(){
   const response = await api.post("/setups", setupDescription)
   const id = response.data._id
   if(id){
-    const file = fileInput.value.files[0];
-    const formData = new FormData();
-    formData.append('image', file);
-    api.post(`/setups/${id}/images`, formData, {
+    snackbar.value = true
+    for (const file of files.value) {
+      const formData = new FormData();
+      formData.set('image', file);
+      api.post(`/setups/${id}/images`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       })
-    router.push("/")
+    }
+    loading.value = false
+    setTimeout(() => {
+      router.push("/")
+    }, 6000)
   }
 }
 
 </script>
 
-<style>
+<style scoped lang="css">
 .link a {
   color: #FFFFFF;
 }
 
+.btn {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    z-index: 1;
+}
+
 </style>
+
